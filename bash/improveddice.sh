93103c1ee9af5b8563b86bcc10da008b8fbe05c2
harmeet <jogarharmeet@gmail.com>
2020-07-22 10:42:43 -0700

    saved changes improved dice

--- bash/improveddice.sh
+++ bash/improveddice.sh
@@ -19,7 +19,7 @@ echo "Rolling..."
 # roll the dice and save the results
 
 #number of dice sides are 6 so we will use 6 as a range
-range=6
+range=20
 
 #putting the minimum value (bias)
 bias=1
@@ -27,15 +27,17 @@ bias=1
 #Now rolling the dice by using range and bias
 die1=$(( RANDOM % range + bias ))
 die2=$(( RANDOM % range + bias ))
+die3=$(( RANDOM % range + bias ))
+
 
 #generating the sum of dice
-sum=$(( die1 + die2 ))
+sum=$(( die1 + die2 + die3))
 
 #generating the average of dice
-average=$(( sum / 2 ))
+average=$(( sum / 3 ))
 
 #now showing the result from dice rolled
-echo "Result $die1, $die2
+echo "Result $die1, $die2, $die3
 
 Sum of dice= $sum
 Average of dice= $average"
